f3ca52cb4a975fd780f9dbb0a01e409bcd5a5601
Ben Walker <ben.walker18@gmail.com>
2020-10-21 10:41:29 -0700

    Tweak social icons styling

--- frontEnd/src/components/uiElements/SocialIcons.tsx
+++ frontEnd/src/components/uiElements/SocialIcons.tsx
@@ -6,15 +6,19 @@ import { Grid } from "@material-ui/core";
 import { connect } from "react-redux";
 
 const styles = (theme: any) => createStyles({
+    hrefText: {
+        color: "#FFFFFF", 
+        textDecoration: 'none'
+    }
 });
 
 interface Props extends WithStyles<typeof styles> { }
 
 const imgSrcs = [
-    { hover: "twitterHover.svg", unHover: "twitter.svg", href: "https://twitter.com" },
-    { hover: "mediumHover.svg", unHover: "medium.svg", href: "https://medium.com" },
-    { hover: "telegramHover.svg", unHover: "telegram.svg", href: "https://telegram.org" },
-    { hover: "discordHover.svg", unHover: "discord.svg", href: "https://discord.com" },
+    { hover: "twitterHover.svg", unHover: "twitter.svg", href: "https://twitter.com", alt: "Twitter" },
+    { hover: "mediumHover.svg", unHover: "medium.svg", href: "https://medium.com", alt: "Medium" },
+    { hover: "telegramHover.svg", unHover: "telegram.svg", href: "https://telegram.org", alt: "Telegram" },
+    { hover: "discordHover.svg", unHover: "discord.svg", href: "https://discord.com", alt: "Discord" },
 ]
 
 const DecoratedSocialIconsClass = withStyles(styles)(
@@ -24,10 +28,10 @@ const DecoratedSocialIconsClass = withStyles(styles)(
             imgSrcs.forEach((imgSrcs, index: number) => {
                 icons.push(
                     <Grid item>
-                        <a href={imgSrcs.href}>
+                        <a className={this.props.classes.hrefText} href={imgSrcs.href}>
                             <img
                                 src={imgSrcs.unHover}
-                                alt={imgSrcs.unHover}
+                                alt={imgSrcs.alt}
                                 onMouseOver={(event) => this.hover(event, index)}
                                 onMouseOut={(event) => this.unHover(event, index)}
                             />
