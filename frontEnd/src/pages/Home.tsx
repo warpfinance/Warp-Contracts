900a5b7fbe65ec16d618001b7d22bdb49fa43110
Ben Walker <ben.walker18@gmail.com>
2020-10-22 15:57:09 -0700

    Add background video

--- frontEnd/src/pages/Home.tsx
+++ frontEnd/src/pages/Home.tsx
@@ -7,6 +7,14 @@ import { Grid } from "@material-ui/core";
 import { connect } from "react-redux";
 
 const styles = (theme: any) => createStyles({
+    backgroundVideo: {
+        position: 'fixed',
+        right: 0,
+        bottom: 0,
+        minWidth: '100%',
+        minHeight: '100%',
+        zIndex: -1,
+    },
     bottomSocial: {
         position: 'absolute',
         left: '50%',
@@ -30,33 +38,38 @@ const DecoratedHomeClass = withStyles(styles)(
     class HomeClass extends React.Component<Props, {}> {
         render() {
             return (
-                <Grid
-                    container
-                    direction="column"
-                    alignItems="center"
-                    spacing={10}
-                >
-                    <Header home={true} />
-                    <div className={this.props.classes.centerButton}>
-                        <Grid
-                            container
-                            direction="column"
-                            alignItems="center"
-                            justify="space-around"
-                            spacing={5}
-                        >
-                            <Grid item>
-                                <img className={this.props.classes.logo} src={"warp logo.svg"} alt={"Warp"}></img>
+                <React.Fragment>
+                    <video autoPlay className={this.props.classes.backgroundVideo} muted loop>
+                        <source src="b2.webm" type="video/webm" />
+                    </video>
+                    <Grid
+                        container
+                        direction="column"
+                        alignItems="center"
+                        spacing={10}
+                    >
+                        <Header home={true} />
+                        <div className={this.props.classes.centerButton}>
+                            <Grid
+                                container
+                                direction="column"
+                                alignItems="center"
+                                justify="space-around"
+                                spacing={5}
+                            >
+                                <Grid item>
+                                    <img className={this.props.classes.logo} src={"warp logo.svg"} alt={"Warp"}></img>
+                                </Grid>
+                                <Grid item>
+                                    <CustomButton href={"/connect"} text={"App"} type={"long"} />
+                                </Grid>
                             </Grid>
-                            <Grid item>
-                                <CustomButton href={"/connect"} text={"App"} type={"long"} />
-                            </Grid>
-                        </Grid>
-                    </div>
-                    <div className={this.props.classes.bottomSocial}>
-                        <SocialIcons />
-                    </div>
-                </Grid >
+                        </div>
+                        <div className={this.props.classes.bottomSocial}>
+                            <SocialIcons />
+                        </div>
+                    </Grid >
+                </React.Fragment>
             )
         }
     }
