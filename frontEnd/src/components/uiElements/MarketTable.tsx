d36a96cb246174329bd1881296b48f8be909ed6e
Ben Walker <ben.walker18@gmail.com>
2020-10-22 09:24:03 -0700

    Replace Material UI table in markets with HTML table to build custom styling on

--- frontEnd/src/components/uiElements/MarketTable.tsx
+++ frontEnd/src/components/uiElements/MarketTable.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { Avatar, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
+import { Avatar, Card, CardContent, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
 import { WithStyles, createStyles, withStyles } from "@material-ui/core/styles";
 
 import { connect } from "react-redux";
@@ -29,101 +29,95 @@ const DecoratedMarketTableClass = withStyles(styles)(
         render() {
             const supplyOrBorrow = this.props.type === "borrower" ? "borrow" : "supply";
             return (
-                <TableContainer>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)} market
+                <table>
+                    <tr>
+                        <th>
+                            <Typography variant="subtitle1" color="textSecondary">
+                                {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)} market
                                     </Typography>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        Total {supplyOrBorrow}
+                        </th>
+                        <th>
+                            <Typography variant="subtitle1" color="textSecondary">
+                                Total {supplyOrBorrow}
+                            </Typography>
+                        </th>
+                        <th>
+                            <Typography variant="subtitle1" color="textSecondary">
+                                {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)}  APY
                                     </Typography>
-                                </TableCell>
-                                <TableCell align="right">
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)}  APY
-                                    </Typography>
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {rows.map((row) => (
-                                <TableRow key={row.name}>
-                                    <TableCell align="left">
-                                        <Grid
-                                            container
-                                            direction="row"
-                                            justify="flex-start"
-                                            alignItems="center"
-                                            spacing={2}
-                                        >
-                                            <Grid item>
-                                                <Avatar alt={row.name} src={row.icon} />
-                                            </Grid>
-                                            <Grid
-                                                item
-                                                direction="column"
-                                            >
-                                                <Grid item>
-                                                    <Typography variant="subtitle1">
-                                                        {row.name}
-                                                    </Typography>
-                                                </Grid>
-                                                <Grid item>
-                                                    <Typography variant="subtitle1" color="textSecondary">
-                                                        {row.supplyShare + "%"}
-                                                    </Typography>
-                                                </Grid>
-                                            </Grid>
+                        </th>
+                    </tr>
+                    {rows.map((row) => (
+                        <tr>
+                            <th>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justify="flex-start"
+                                    alignItems="center"
+                                    spacing={2}
+                                >
+                                    <Grid item>
+                                        <Avatar alt={row.name} src={row.icon} />
+                                    </Grid>
+                                    <Grid
+                                        item
+                                        direction="column"
+                                    >
+                                        <Grid item>
+                                            <Typography variant="subtitle1">
+                                                {row.name}
+                                            </Typography>
                                         </Grid>
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <Grid
-                                            container
-                                            direction="column"
-                                            justify="center"
-                                            alignItems="center"
-                                        >
-                                            <Grid item>
-                                                <Typography variant="subtitle1">
-                                                    {row.totalSupply.toLocaleString() + " " + row.totalSupplyCurrency}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant="subtitle1" color={row.totalSupplyChange > 0 ? "secondary" : "error"} >
-                                                    {row.totalSupplyChange > 0 ? `+${row.totalSupplyChange}%` : `${row.totalSupplyChange}%`}
-                                                </Typography>
-                                            </Grid>
+                                        <Grid item>
+                                            <Typography variant="subtitle1" color="textSecondary">
+                                                {row.supplyShare + "%"}
+                                            </Typography>
                                         </Grid>
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <Grid
-                                            container
-                                            direction="column"
-                                            justify="center"
-                                            alignItems="center"
-                                        >
-                                            <Grid item>
-                                                <Typography variant="subtitle1">
-                                                    {row.supplyApy + "%"}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant="subtitle1" color={row.supplyApychange > 0 ? "secondary" : "error"} >
-                                                    {row.supplyApychange > 0 ? `+${row.supplyApychange}%` : `${row.supplyApychange}%`}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                                    </Grid>
+                                </Grid>
+                            </th>
+                            <th>
+                                <Grid
+                                    container
+                                    direction="column"
+                                    justify="center"
+                                    alignItems="center"
+                                >
+                                    <Grid item>
+                                        <Typography variant="subtitle1">
+                                            {row.totalSupply.toLocaleString() + " " + row.totalSupplyCurrency}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subtitle1" color={row.totalSupplyChange > 0 ? "secondary" : "error"} >
+                                            {row.totalSupplyChange > 0 ? `+${row.totalSupplyChange}%` : `${row.totalSupplyChange}%`}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </th>
+                            <th>
+                                <Grid
+                                    container
+                                    direction="column"
+                                    justify="center"
+                                    alignItems="center"
+                                >
+                                    <Grid item>
+                                        <Typography variant="subtitle1">
+                                            {row.supplyApy + "%"}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subtitle1" color={row.supplyApychange > 0 ? "secondary" : "error"} >
+                                            {row.supplyApychange > 0 ? `+${row.supplyApychange}%` : `${row.supplyApychange}%`}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </th>
+                        </tr>
+                    ))}
+                </table>
             );
         }
     }
