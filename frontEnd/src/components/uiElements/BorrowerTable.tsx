62a0a49ad23044a5551e23d110cdf3b5615cbba7
Ben Walker <ben.walker18@gmail.com>
2020-10-23 11:13:30 -0700

    Add remaining UI elements to borrower & lender pages, except the input fields

--- frontEnd/src/components/uiElements/BorrowerTable.tsx
+++ frontEnd/src/components/uiElements/BorrowerTable.tsx
@@ -3,24 +3,39 @@ import * as React from "react";
 import { Avatar, Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@material-ui/core";
 import { WithStyles, createStyles, withStyles } from "@material-ui/core/styles";
 
+import { AvatarGroup } from "@material-ui/lab";
 import { CustomButton } from ".."
 import { connect } from "react-redux";
 
 //@ts-ignore
-function createData(icon, wallet, walletCurrency) {
-    return { icon, wallet, walletCurrency, };
+function createBorrowData(icon, name, supplyShare, amount, currency, lp, lpCurrency) {
+    return { icon, name, supplyShare, amount, currency, lp, lpCurrency };
 }
 
-const lendData = [
-    createData(<Avatar alt={"dai.png"} src={"dai.png"} />, 100, "DAI"),
-    createData(<Avatar alt={"usdt.png"} src={"usdt.png"} />, 0, "USDT"),
-    createData(<Avatar alt={"usd.png"} src={"usd.png"} />, 0, "USDC"),
+//@ts-ignore
+function createRepayData(icon, amount, currency) {
+    return { icon, amount, currency, };
+}
+
+const borrowData = [
+    createBorrowData(<AvatarGroup max={2}><Avatar alt={"eth.png"} src={"eth.png"} />
+        <Avatar alt={"dai.png"} src={"dai.png"} /></AvatarGroup>,
+        "ETH - DAI", 1.97, 765, "USD", 400, "LP"),
+    createBorrowData(<AvatarGroup><Avatar alt={"eth.png"} src={"eth.png"} />
+        <Avatar alt={"usdt.png"} src={"usdt.png"} /></AvatarGroup>,
+        "ETH - USDT", 3.25, 345, "USD", 400, "LP"),
+    createBorrowData(<AvatarGroup><Avatar alt={"wbtc.png"} src={"wbtc.png"} />
+        <Avatar alt={"weth.png"} src={"weth.png"} /></AvatarGroup>,
+        "wBTC - wETH", 1.32, 765, "USD", 400, "LP"),
+    createBorrowData(<AvatarGroup><Avatar alt={"usdt.png"} src={"usdt.png"} />
+        <Avatar alt={"weth.png"} src={"weth.png"} /></AvatarGroup>,
+        "USDT - wETH", 2.18, 456, "USD", 400, "LP"),
 ];
 
-const withdrawData = [
-    createData(<Avatar alt={"dai.png"} src={"dai.png"} />, 100, "DAI"),
-    createData(<Avatar alt={"usdt.png"} src={"usdt.png"} />, 25, "USDT"),
-    createData(<Avatar alt={"usd.png"} src={"usd.png"} />, 68, "USDC"),
+const repayData = [
+    createRepayData(<Avatar alt={"dai.png"} src={"dai.png"} />, 100, "DAI"),
+    createRepayData(<Avatar alt={"usdt.png"} src={"usdt.png"} />, 249, "USDT"),
+    createRepayData(<Avatar alt={"usd.png"} src={"usd.png"} />, 68, "USDC"),
 ];
 
 const styles = (theme: any) => createStyles({
@@ -33,8 +48,9 @@ interface Props extends WithStyles<typeof styles> {
 const DecoratedBorrowerTableClass = withStyles(styles)(
     class BorrowerTableClass extends React.Component<Props, {}> {
         render() {
-            const data = this.props.type === "repay" ? withdrawData : lendData;
+            const data = this.props.type === "repay" ? repayData : borrowData;
             const repayOrBorrow = this.props.type === "repay" ? "repay" : "borrow";
+            const availableOrAmountDue = this.props.type === "repay" ? "Amount due" : "Available";
 
             return (
                 <Grid
@@ -49,12 +65,12 @@ const DecoratedBorrowerTableClass = withStyles(styles)(
                                     <TableCell>
                                         <Typography variant="subtitle1" color="textSecondary">
                                             {this.props.type.charAt(0).toUpperCase() + this.props.type.slice(1)}
-                                    </Typography>
+                                        </Typography>
                                     </TableCell>
                                     <TableCell>
                                         <Typography variant="subtitle1" color="textSecondary">
-                                            Wallet
-                                    </Typography>
+                                            {availableOrAmountDue}
+                                        </Typography>
                                     </TableCell>
                                     <TableCell>
                                         <Typography variant="subtitle1" color="textSecondary">
@@ -64,7 +80,7 @@ const DecoratedBorrowerTableClass = withStyles(styles)(
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {data.map((row) => (
+                                {data.map((row: any) => (
                                     <TableRow>
                                         <TableCell>
                                             <Grid
@@ -77,7 +93,7 @@ const DecoratedBorrowerTableClass = withStyles(styles)(
 
                                                 {row.icon}
                                                 <Typography variant="subtitle1">
-                                                    {row.walletCurrency}
+                                                    {this.props.type === "repay" ? row.currency : row.name}
                                                 </Typography>
                                             </Grid>
                                         </TableCell>
@@ -90,9 +106,17 @@ const DecoratedBorrowerTableClass = withStyles(styles)(
                                             >
                                                 <Grid item>
                                                     <Typography variant="subtitle1">
-                                                        {row.wallet.toLocaleString() + " " + row.walletCurrency}
+                                                        {row.amount.toLocaleString() + " " + row.currency}
                                                     </Typography>
                                                 </Grid>
+                                                {this.props.type === "borrow" ?
+                                                    <Grid item>
+                                                        <Typography color="textSecondary">
+                                                            {row.lp + " " + row.lpCurrency}
+                                                        </Typography>
+                                                    </Grid> :
+                                                    null
+                                                }
                                             </Grid>
                                         </TableCell>
                                         <TableCell>
