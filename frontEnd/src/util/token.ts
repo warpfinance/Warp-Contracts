8db084b7ea3ca3233508f193bf8958359f6a73b2
Remscar <remscar@gmail.com>
2020-11-06 14:59:02 -0800

    Fixed image names

--- frontEnd/src/util/token.ts
+++ frontEnd/src/util/token.ts
@@ -6,10 +6,10 @@ const tokenImages: {[key: string]: string | string[]} = {
   '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48': 'usdc.png',
   '0x2F375e94FC336Cdec2Dc0cCB5277FE59CBf1cAe5': 'usdc.png',
   '0xdac17f958d2ee523a2206206994597c13d831ec7': 'usdt.png',
-  '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11': ['eth.png', 'dai.ping'],
-  '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852': ['eth.png', 'usdt.ping'],
-  '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc': ['eth.png', 'usdc.ping'],
-  '0xbb2b8038a1640196fbe3e38816f3e67cba72d940': ['eth.png', 'wbtc.ping'],
+  '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11': ['eth.png', 'dai.png'],
+  '0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852': ['eth.png', 'usdt.png'],
+  '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc': ['eth.png', 'usdc.png'],
+  '0xbb2b8038a1640196fbe3e38816f3e67cba72d940': ['eth.png', 'wbtc.png'],
   
 }
 
