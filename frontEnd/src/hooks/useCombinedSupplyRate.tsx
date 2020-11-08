bcde502b726ac1fca1113495b4e617df0d7ebc8e
Remscar <remscar@gmail.com>
2020-11-07 16:27:03 -0800

    Finished Dashboard

---
+++ frontEnd/src/hooks/useCombinedSupplyRate.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { StableCoinWarpVaultService } from "../services/stableCoinWarpVault";
+import { WarpControlService } from "../services/warpControl";
+import { calculateAPYFromRate } from "../util/interest";
+import { getLogger } from "../util/logger";
+import { Token } from "../util/token";
+import { parseBigNumber } from "../util/tools";
+import { ConnectedWeb3Context } from "./connectedWeb3";
+
+
+const logger = getLogger('Hooks::useCombinedSupplyRate');
+
+export const useCombinedSupplyRate = (context: ConnectedWeb3Context, control: WarpControlService, stableCoins: Token[]) => {
+
+    const [totalInterestRate, setTotalInterestRate] = React.useState(0);
+
+    React.useEffect(() => {
+      let isSubscribed = true;
+    
+      const calculateRate = async() => {
+          if (!context.account || !control) {
+              return;
+          }
+    
+          logger.log("Calculating total interest rate.");
+    
+          interface SupplyStats {
+              amount: number;
+              rate: number;
+          }
+    
+          const supplyStats: SupplyStats[] = [];
+    
+          for (const token of stableCoins) {
+              const targetVault = await control.getStableCoinVault(token.address);
+              const vault = new StableCoinWarpVaultService(context.library, context.account, targetVault);
+    
+              const stats: SupplyStats = {
+                  rate: calculateAPYFromRate(parseBigNumber(await vault.supplyRate())),
+                  amount: parseBigNumber(await vault.getBalance(context.account), token.decimals)
+              }
+    
+              supplyStats.push(stats);
+          }
+
+          const totalBorrowedAmount = supplyStats.reduce((a: number, b: SupplyStats) => {
+              return a + b.amount;
+          }, 0);
+
+          let calculatedRate = 0;
+          for (const stats of supplyStats) {
+            calculatedRate = calculatedRate + (stats.amount / totalBorrowedAmount) * stats.rate;
+          }
+
+          if (isSubscribed) {
+              setTotalInterestRate(calculatedRate);
+          }
+      }
+      calculateRate();
+    
+      return () => {
+          isSubscribed = false;
+      }
+      
+    
+    }, [context.account, context.library, context.networkId, stableCoins, control]);
+
+    return totalInterestRate;
+}
--- frontEnd/src/pages/App/Dashboard.tsx
+++ frontEnd/src/pages/App/Dashboard.tsx
@@ -7,6 +7,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useStableCoinTokens } from "../../hooks/useStableCoins";
 import { useConnectedWeb3Context } from "../../hooks/connectedWeb3";
 import { useLPTokens } from "../../hooks/useLPTokens";
+import { useTotalLentAmount } from "../../hooks/useTotalLentAmount";
+import { useUSDCToken } from "../../hooks/useUSDC";
+import { useWarpControl } from "../../hooks/useWarpControl";
+import { useBorrowLimit } from "../../hooks/useBorrowLimit";
+import { useCombinedBorrowRate } from "../../hooks/useCombinedBorrowRate";
+import { useCombinedSupplyRate } from "../../hooks/useCombinedSupplyRate";
+import { parseBigNumber } from "../../util/tools";
 
 const useStyles = makeStyles(theme => ({
     progress: {
@@ -17,13 +24,7 @@ const useStyles = makeStyles(theme => ({
 interface Props { }
 
 // TO-DO: Web3 integration
-const data = {
-    borrowBalance: 0.00,
-    borrowLimitPercentage: 0,
-    borrowLimitUsd: 0,
-    lendingBalance: 0.00,
-    netApy: 25,
-}
+
 
 export const Dashboard: React.FC<Props> = (props: Props) => {
     const classes = useStyles();
@@ -31,6 +32,42 @@ export const Dashboard: React.FC<Props> = (props: Props) => {
     const context = useConnectedWeb3Context();
     const stableCoins = useStableCoinTokens(context);
     const lpTokens = useLPTokens(context);
+    const {control} = useWarpControl(context);
+    const usdc = useUSDCToken(context);
+    const supplyBalance = useTotalLentAmount(context, control, usdc);
+    const {totalBorrowedAmount, borrowLimit} = useBorrowLimit(context, control);
+    const combinedBorrowRate = useCombinedBorrowRate(context, control, stableCoins);
+    const combinedSupplyRate = useCombinedSupplyRate(context, control, stableCoins);
+
+    let borrowBalanceUSDC = 0;
+    let borrowLimitUSDC = 0;
+    let borrowPercent = 0;
+
+    if (usdc) {
+        borrowBalanceUSDC = parseBigNumber(totalBorrowedAmount, usdc?.decimals);
+        borrowLimitUSDC = parseBigNumber(borrowLimit, usdc?.decimals);
+        if (borrowLimitUSDC > borrowBalanceUSDC) {
+            borrowPercent = (borrowBalanceUSDC / borrowLimitUSDC) * 100;
+        } else {
+            borrowPercent = 100;
+        }
+    }
+
+    let netApy = 0;
+
+    if (borrowBalanceUSDC > 0 || supplyBalance > 0) {
+        const totalAmount = borrowBalanceUSDC + supplyBalance;
+        netApy = combinedSupplyRate * (supplyBalance / totalAmount) - combinedBorrowRate * (borrowBalanceUSDC / totalAmount);
+        netApy = netApy * 100;
+    }
+
+    const data = {
+        borrowBalance: borrowBalanceUSDC,
+        borrowLimitPercentage: borrowPercent,
+        borrowLimitUsd: borrowLimitUSDC,
+        lendingBalance: supplyBalance,
+        netApy,
+    }
 
     return (
         <Grid
@@ -51,7 +88,7 @@ export const Dashboard: React.FC<Props> = (props: Props) => {
                     <InformationCard header="Lending balance" text={`$${data.lendingBalance.toFixed(2)}`} />
                 </Grid>
                 <Grid item>
-                    <InformationCard header="Net APY" text={`${data.netApy.toLocaleString()}%`} />
+                    <InformationCard header="Net APY" text={`${data.netApy.toLocaleString(undefined, {maximumFractionDigits: 2})}%`} />
                 </Grid>
                 <Grid item>
                     <InformationCard header="Borrow balance" text={`$${data.borrowBalance.toFixed(2)}`} />
@@ -66,7 +103,7 @@ export const Dashboard: React.FC<Props> = (props: Props) => {
             >
                 <Grid item>
                     <Typography variant="subtitle1" color="textSecondary">
-                        {`${data.borrowLimitPercentage}%`}
+                        {`${data.borrowLimitPercentage.toFixed(0)}%`}
                     </Typography>
                 </Grid>
                 <div className={classes.progress}>
@@ -74,7 +111,9 @@ export const Dashboard: React.FC<Props> = (props: Props) => {
                 </div>
                 <Grid item>
                     <Typography variant="subtitle1" color="textSecondary">
-                        {`$${data.borrowLimitUsd}`}
+                        {`$${data.borrowLimitUsd.toLocaleString(undefined, {
+                            maximumFractionDigits: 0
+                        })}`}
                     </Typography>
                 </Grid>
             </Grid>
