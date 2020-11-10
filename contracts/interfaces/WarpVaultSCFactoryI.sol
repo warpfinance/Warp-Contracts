119469b105b1c5b1e19fe3d60219de8060d5c5b1
stan36 <decentracorp@pm.me>
2020-11-10 14:33:26 -0800

    fixed redeem to use _amount in stablecoin, added events and fee logic

--- contracts/interfaces/WarpVaultSCFactoryI.sol
+++ contracts/interfaces/WarpVaultSCFactoryI.sol
@@ -15,11 +15,13 @@ abstract contract WarpVaultSCFactoryI {
   @notice createNewWarpVaultSC is used to create new WarpVaultSC contract instances
   @param _InterestRate is the address of the InterestRateModel contract created for this Warp Vault
   @param _StableCoin is the address of the stablecoin contract this WarpVault will manage
+  @param _warpTeam is the address of the Warp Team used for fees
   @param _initialExchangeRate is the exchange rate mantissa used to determine the initial exchange rate of stablecoin to warp stablecoin
     **/
     function createNewWarpVaultSC(
         address _InterestRate,
         address _StableCoin,
+        address _warpTeam,
         uint256 _initialExchangeRate
     ) public virtual returns (address);
 }
